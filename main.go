187ce214ee59c585de2baaa8071f9fa345ba0ff9
Takayuki Maeda <maeda.takayuki@moneyforward.co.jp>
2019-12-12 15:17:23 +0900

    Create main.go

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"time"
+
+	"github.com/cheggaaa/pb/v3"
+	yaml "gopkg.in/yaml.v2"
+)
+
+type Date struct {
+	Members []string `yaml:"members"`
+}
+
+func main() {
+	s := rand.NewSource(time.Now().Unix())
+	r := rand.New(s)
+
+	buf, err := ioutil.ReadFile("members.yml")
+	if err != nil {
+		panic(err)
+	}
+
+	var data Date
+	err = yaml.Unmarshal(buf, &data)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("選ばれし者を探しています...")
+
+	count := 2000
+	bar := pb.StartNew(count)
+
+	for i := 0; i < count; i++ {
+		bar.Increment()
+		time.Sleep(time.Millisecond)
+	}
+	bar.Finish()
+
+	fmt.Printf("選ばれしものは%vでした！！！", data.Members[r.Intn(len(data.Members))])
+}
